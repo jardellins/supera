d47f90a3376fd619e4e7c102511426bd65e09ad0
Jardel Lins <jardel.a7x@gmail.com>
2021-07-29 22:45:18 -0300

    Create page info

--- src/components/ListGames.js
+++ src/components/ListGames.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Image, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+} from "react-native";
+import { useNavigation } from "@react-navigation/core";
 
-import images from '../services/images'
+
+import images from "../services/images";
 import colors from "../styles/colors";
 import fonts from "../styles/fonts";
 
-const ListGames = ({ title, price, image }) => {
-    
-return (
-    <TouchableOpacity activeOpacity={0.7}>
+const ListGames = ({ title, price }) => {
+  const navigation = useNavigation()
+
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      onPress={() => navigation.navigate("Info", {title, price})}
+    >
       <View style={styles.container}>
         <Text style={styles.title}>{title}</Text>
         <Image
@@ -25,34 +38,34 @@ return (
 export default ListGames;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignContent: 'center',
-        justifyContent: 'flex-end',
-        marginVertical: 20,
-        paddingHorizontal: 10,
-        width: Dimensions.get('window').width * 0.45,
-        backgroundColor: colors.shape,
-        marginHorizontal: 8,
-      },
-      image: {
-        height: Dimensions.get('window').height * 0.2,
-        alignSelf: 'center'
-    },
-    title: {
-      fontFamily: fonts.heading,
-      fontSize: 18,
-      color: colors.heading,
-      textAlign: 'center',
-      width: '80%',
-      alignSelf: 'center',
-      marginHorizontal: 7,
-    },
-    price: {
-      fontFamily: fonts.text,
-      fontSize: 17,
-      fontWeight: 'bold',
-      textAlign: 'center',
-      marginBottom: 8,
-    },
-})
+  container: {
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "flex-end",
+    marginVertical: 20,
+    paddingHorizontal: 10,
+    width: Dimensions.get("window").width * 0.45,
+    backgroundColor: colors.shape,
+    marginHorizontal: 8,
+  },
+  image: {
+    height: Dimensions.get("window").height * 0.2,
+    alignSelf: "center",
+  },
+  title: {
+    fontFamily: fonts.heading,
+    fontSize: 18,
+    color: colors.heading,
+    textAlign: "center",
+    width: "80%",
+    alignSelf: "center",
+    marginHorizontal: 7,
+  },
+  price: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 8,
+  },
+});
